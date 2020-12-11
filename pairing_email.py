96bce511bf8bf4ae508edfa1516219e54dda72dc
Sarah Zeng <69217632+sayzeng@users.noreply.github.com>
2020-12-11 14:34:45 -0800

    Add files via upload

--- pairing_email.py
+++ pairing_email.py
@@ -9,95 +9,75 @@ Purpose: Notification of pairing
 """
 
 import pandas as pd
-import numpy as np
 import smtplib
+import getpass
 from email.message import EmailMessage
 
-#email credentials
-me=input('Enter your email address:')
-pwd=input('Enter your password:')
-#definitions
-pairsheet='pairs.xlsx'
+# Email credentials
+me = input('Enter your email address:\n')
+pwd = getpass.getpass(prompt='Enter your password:\n')
 
-#login to server
+# Login to server
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login(me,pwd)
+server.login(me, pwd)
 
-#import pair and contact information
-df0=pd.read_excel(pairsheet)
-pairs=df0['Pair'].max()
+# Import pair and contact information
+pairsheet = 'pairs.xlsx'
+df = pd.read_excel(pairsheet)
+pairCount = df['Pair'].max()
 
-#misc
-com=', '
-tab='\t'
-nsep='\n'
-nnsep='\n \n'
+# Housecleaning
+df.Year.where(df.Year==1, 0, inplace=True)
+df = df.sort_values(['Pair', 'Year']).set_index(['Pair', 'Year'])
+name = df['First Name'] + ' ' + df['Last Name']
+df.insert(0, 'Name', name)
+df.drop(['First Name', 'Last Name'], inplace=True, axis=1)
 
-#seed
-df=pd.DataFrame()
-columns=['PairID','contactinfo','sendto']
+def compose_contact_info(df, pair):
+    """ Creates block of text with given pair's contact information """
+    mentor = df.loc[pair, 0].dropna()
+    mentee = df.loc[pair, 1].dropna()
+    contact_info = mentor.to_string() + '\n\n' + mentee.to_string()
+    return contact_info
 
-#START HERE #spit out: string of to emails (you), table for email body (line2)
-for i in range(1,pairs+1):
-    pairinfo=df0.loc[df0['Pair']==i].replace(np.nan,'N/A')
+def sendto(df, pair):
+    """ Returns string of email addresses for given pair """
+    recipients = df.loc[pair][['UCSD Email', 'Other Email']].stack()
+    return ', '.join(list(recipients))
+
+# Load in text for the email body
+with open('intro_message.txt', 'r') as file:
+    email_text = file.read()
+
+for pair in range(1, pairCount + 1):
+    # Preview email
+    recipients = sendto(df, pair)
+    contact_info = compose_contact_info(df, pair)
+    email_body = email_text.format(contact_info, me)
+    print(email_body)
     
-    n='Name: '
-    n1=pairinfo.iloc[0]['First Name']+' '+pairinfo.iloc[0]['Last Name']
-    n2=pairinfo.iloc[1]['First Name']+' '+pairinfo.iloc[1]['Last Name']
-    p='Phone: '
-    p1=pairinfo.iloc[0]['Phone Number']
-    p2=pairinfo.iloc[1]['Phone Number']
-    e='Email: '
-    e1a=pairinfo.iloc[0]['UCSD Email']
-    e1b=pairinfo.iloc[0]['Other Email']
-    e2a=pairinfo.iloc[1]['UCSD Email']
-    e2b=pairinfo.iloc[1]['Other Email']
-    e1=e1a+com+e1b
-    e2=e2a+com+e2b
-    
-    c1=n+tab+n1+nsep +p+tab+p1+nsep +e+tab+e1
-    c2=n+tab+n2+nsep +p+tab+p2+nsep +e+tab+e2
-    
-    no=pairinfo.iloc[0]['Pair']
-    c=c1+nsep+nsep+c2
-    sendto=e1+com+e2
-    
-    row=pd.DataFrame([no,c,sendto]).transpose()
-    df=df.append(row)
-
-df.columns=columns  
-df=df.reset_index(drop=True)
-print('Ready to construct emails.\n')
-
-#iteration
-for i in range(pairs):
-    #email formatting
+    # Compose email
     msg = EmailMessage()
-    msg['Subject']='You have a COMRADE!'
-    msg['From']=me
+    msg['Subject'] = 'You have a COMRADE!'
+    msg['From'] = me
+    msg['To'] = recipients
+    msg.set_content(email_body)
     
-    #compose email body
-    line1='Welcome to the UCSD Economics COMRADES mentorship program! I hope that you both find your mentorship experience rewarding and beneficial. You can find each other’s contact information below:'
-    line3='For the month of September, you each have $5 ($10 total) to spend on coffee or froyo or any other kind of bonding consumption good (except alcohol, sorry!). To submit a reimbursement, please take a photo of your itemised receipt and send it to '+me+' with the string ‘COMRADES’ in the subject line.'
-    line4='\n Sarah'
+    # Verification
+    send_text = f'Would you like to send this email to [{recipients}]? (Y/N/stop)\n'
+    send_email = input(send_text)
     
-    #iterated content
-    msg['To']=df.iloc[i]['sendto']
-    line2=df.iloc[i]['contactinfo']
-    lines=(line1,line2,line3,line4)
-    body=nnsep.join(lines)
-    msg.set_content(body)
-    print(body)
-    
-    #Warning input
-    warningtxt='Are you sure you want to send an email to '+df.iloc[i]['sendto']+' ? (Y/N)\n'
-    send_email=input(warningtxt)
-    
-    if send_email=='Y':
-        server.send_message(msg)
-        print('\n\n Email sent! \n\n\n')
-    elif send_email=='break':
+    if send_email == 'Y':
+        send_email = input('Are you sure you would like to send this message?\n')
+        if send_email == 'Y':
+            server.send_message(msg)
+            print('\n\n Email sent! \n\n\n')
+        else:
+            break
+    elif send_email == 'stop':
         break
     else:
-        print('\n\n Check your code again.')
+        print('\n\n Email not sent.\n\n')
+        
+print('\nCode complete.')
